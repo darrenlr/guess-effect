3a3b6507c7fb114ca1aebe79fa0170bde696ded8
Darren Russell <48918857+darrenlr@users.noreply.github.com>
2023-08-03 17:05:08 +0000

    modify release date display

--- components/ReleaseDate.js
+++ components/ReleaseDate.js
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "../styles/ReleaseDate.module.css";
 
 const ReleaseDate = ({ date }) => {
-	return (
-		<>
-			<h4 className={styles.releaseDate}>Release Date: {date}</h4>
-		</>
-	);
+  const newDate = new Date(date);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const dateString = newDate.toLocaleDateString("en-US", options);
+
+  return (
+    <>
+      <h4 className={styles.releaseDate}>Release Date: {dateString}</h4>
+    </>
+  );
 };
 
 export default ReleaseDate;
