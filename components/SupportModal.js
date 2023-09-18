004a5e45ffc1270dbf34dd1daa82e3448d681853
Darren Russell <48918857+darrenlr@users.noreply.github.com>
2023-09-18 18:03:08 +0000

    update support modal

--- components/SupportModal.js
+++ components/SupportModal.js
@@ -1,4 +1,4 @@
-import styles from "../styles/GameOverModal.module.css";
+import styles from "../styles/Modal.module.css";
 
 const SupportModal = ({ closeModal }) => {
 
@@ -13,8 +13,11 @@ const SupportModal = ({ closeModal }) => {
 			<div className={styles.modal}>
 				<div className={styles.modalContainer}>
 					<h3>Support</h3>
-					<p>Thank you so much for playing!</p>
-					<p>If you&apos;ve enjoyed, please consider supporting, this will give you access to the game archives when they go live!</p>
+					<div>Thank you so much for playing!</div>
+					<div>If you&apos;ve enjoyed, please consider supporting, this will give you access to the game archives when they go live!</div>
+					<a href='https://ko-fi.com/S6S7EDM09' target='_blank' rel='noreferrer noopener'>
+						<img height='36' style={{border: '0px', height: '36px'}} src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' alt='Buy Me a Coffee at ko-fi.com' />
+					</a>
 				</div>
 			</div>
 		</div>
