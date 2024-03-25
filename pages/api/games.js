c475e2974bd7b5fdab7ff3b039ea90239463136a
Darren Russell <48918857+darrenlr@users.noreply.github.com>
2024-03-25 19:38:17 +0000

    add debugging to search api

--- pages/api/games.js
+++ pages/api/games.js
@@ -4,11 +4,22 @@ const { serverRuntimeConfig } = getConfig();
 
 export default async (req, res) => {
   const { search } = req.query;
-  
+
   const apiKey = serverRuntimeConfig.RAWG_API_KEY;
 
   const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}&search=${search}`);
-  const data = await response.json();
 
-  res.status(200).json(data);
+  console.log("Response status:", response.status);
+  console.log("Response headers:", response.headers);
+
+  const text = await response.text();
+
+  // Then try to parse it as JSON
+  try {
+    const data = JSON.parse(text);
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("JSON parsing error:", error);
+    res.status(500).json({ message: "Error parsing JSON response from server.", error: error.toString() });
+  }
 };
