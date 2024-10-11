a6fd7c2efefc0b8df078b5dd2b23e2c800c85c4b
Darren Russell <48918857+darrenlr@users.noreply.github.com>
2024-10-11 16:56:50 +0000

    update gameOverModal

--- components/GameOverModal.js
+++ components/GameOverModal.js
@@ -1,25 +1,73 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import usePlayerStats from "../hooks/usePlayerStats";
 import styles from "../styles/GameOverModal.module.css";
 import confetti from "canvas-confetti";
 
 const GameOverModal = ({ show, gameTitle, score, gamesPlayed, highestScore, averageScore, gamesWon, globalAverageScore, globalAverageGuesses, globalWinners, playerCount, gameWon, onClose }) => {
-	useEffect(() => {
-		if (show && gameWon) {
-			const confettiAnimation = confetti.create(undefined, {
-			  resize: true,
-			  useWorker: true,
-			});
-			confettiAnimation({
-			  zIndex: 101,
-			  particleCount: 100,
-			  spread: 60,
-			  origin: { y: 0.6 },
-			});
-		  }
-		}, [show, gameWon]);
+	const [animatedScore, setAnimatedScore] = useState(0);
+	const [isScoreAnimationDone, setIsScoreAnimationDone] = useState(false);
+	const [highestScoreScale, setHighestScoreScale] = useState(1);
 
-	if (!show) {
+	useEffect(() => {
+        if (show) {
+            const duration = 10000; 
+            const increment = score / (duration / 100); 
+            let currentScore = 0;
+
+            const animateScore = () => {
+                if (currentScore < score) {
+                    currentScore += increment;
+                    if (currentScore > score) {
+                        currentScore = score; 
+                    }
+                    setAnimatedScore(Math.floor(currentScore));
+
+                    requestAnimationFrame(animateScore); 
+                } else {
+                    setIsScoreAnimationDone(true);
+                }	
+            };
+            animateScore();
+        }
+    }, [show, score]);
+
+	useEffect(() => {
+        if (isScoreAnimationDone && show) {
+            const confettiAnimation = confetti.create(undefined, {
+                resize: true,
+                useWorker: true,
+            });
+
+            if (gameWon) {
+                confettiAnimation({
+                    zIndex: 101,
+                    particleCount: 1000,
+                    spread: 80,
+                    origin: { y: 0.4, x: 0.5 },
+                    scalar: 0.6,
+                });
+            } else {
+                confettiAnimation({
+                    zIndex: 101,
+                    particleCount: 500,
+                    spread: 80,
+                    colors: ['#FF4500', '#FF6347', '#FFD700', '#FF8C00'],
+                    origin: { y: 0.4, x: 0.5 },
+                    scalar: 0.4,
+                    drift: 0.5,
+                });
+            }
+
+            if (gameWon && score > highestScore) {
+                setHighestScoreScale(2.0); 
+                setTimeout(() => setHighestScoreScale(1), 600);
+			}
+            
+        }
+    }, [isScoreAnimationDone, show, gameWon, score, highestScore]);
+
+
+	if (! show) {
 		return null;
 	}
 
@@ -32,16 +80,16 @@ const GameOverModal = ({ show, gameTitle, score, gamesPlayed, highestScore, aver
 				</h1>			
 				<p>the game was:</p>
 				<h2 style={{ textAlign: 'center' }}>{gameTitle}</h2>
-			  	<h3>Score: {score}</h3>
+			  	<h3>Score: {animatedScore}</h3>
 			  <div className={styles.statsRow}  style={{ marginBottom: '1.5rem', marginTop: '1rem'}}>
 				<div>
 				  <p>{gamesPlayed}</p>
 				  <span>Played</span>
 				</div>
-				<div>
-				  <p>{highestScore}</p>
-				  <span>High</span>
-				</div>
+				<div style={{ transform: `scale(${highestScoreScale})`, transition: 'transform 0.3s ease' }}>
+                    <p>{highestScore}</p>
+                    <span>High</span>
+                </div>
 				<div>
 				  <p>{averageScore}</p>
 				  <span>Avg</span>
