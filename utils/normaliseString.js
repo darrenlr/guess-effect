64f1cf3ad5ab743c1d3303f160d66edb57da1f3f
Darren Russell <48918857+darrenlr@users.noreply.github.com>
2025-08-06 07:38:28 +0000

    - update string normalization

--- utils/normaliseString.js
+++ utils/normaliseString.js
@@ -1,8 +1,9 @@
-export function normaliseString(str){
+export function normaliseString(str) {
   return str
     .normalize('NFKC')
-    .replace(/[\u00A0\u2000-\u200B\u202F\u205F\u3000]/g, ' ') // Replace non-standard spaces with normal space
+    .replace(/[\u2010-\u2015]/g, '-')                         // Normalize all dash variants to -
+    .replace(/[\u00A0\u2000-\u200B\u202F\u205F\u3000]/g, ' ') // Normalize weird spaces
     .replace(/\s+/g, ' ')                                     // Collapse multiple spaces
-    .trim()                                                   // Trim leading/trailing
-    .toLowerCase();                                           // Make lowercase
+    .trim()                                                   // Trim leading/trailing spaces
+    .toLowerCase();                                           // Lowercase
 }
