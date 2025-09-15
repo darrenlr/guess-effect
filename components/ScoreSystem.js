52e49054b146abc77d95082dcd19376d9c907242
Darren Russell <48918857+darrenlr@users.noreply.github.com>
2025-09-15 08:15:33 +0000

    - normalise single quotes

--- components/ScoreSystem.js
+++ components/ScoreSystem.js
@@ -11,17 +11,17 @@ import { normaliseString } from '../utils/normaliseString';
 import styles from "../styles/ScoreSystem.module.css";
 
 import { doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
-import { db } from '../utils/firebase'; 
+import { db } from '../utils/firebase';
 
 const getCollectionName = () => {
-	const branch = process.env.NEXT_PUBLIC_BRANCH || 'main'; 
-  
+	const branch = process.env.NEXT_PUBLIC_BRANCH || 'main';
+
 	if (branch === 'release') {
-	  return "playerStats-release";
+		return "playerStats-release";
 	}
-  
+
 	return "playerStats";
-  };
+};
 
 const initialGameState = {
 	date: null,
@@ -52,29 +52,29 @@ const trackPlayer = async (finalScore, usedGuesses, hasWon) => {
 	const docRef = doc(db, collectionName, today);
 	const docSnap = await getDoc(docRef);
 	let winner = hasWon ? 1 : 0;
-  
+
 	if (docSnap.exists()) {
-	  await updateDoc(docRef, { 
-		count: docSnap.data().count + 1 ,
-		totalScore: docSnap.data().totalScore + finalScore,
-		totalGuesses: docSnap.data().totalGuesses + usedGuesses,
-		totalWinners: docSnap.data().totalWinners + winner,
-		highScore: finalScore > docSnap.data().highScore ? finalScore : docSnap.data().highScore,
-	});
+		await updateDoc(docRef, {
+			count: docSnap.data().count + 1,
+			totalScore: docSnap.data().totalScore + finalScore,
+			totalGuesses: docSnap.data().totalGuesses + usedGuesses,
+			totalWinners: docSnap.data().totalWinners + winner,
+			highScore: finalScore > docSnap.data().highScore ? finalScore : docSnap.data().highScore,
+		});
 	} else {
-	  // Create a new document for today with count 1
-	  await setDoc(docRef, { 
-		count: 1,
-		totalScore: finalScore,
-		totalGuesses: usedGuesses,
-		totalWinners: winner,
-		highScore: finalScore,
-	});
+		// Create a new document for today with count 1
+		await setDoc(docRef, {
+			count: 1,
+			totalScore: finalScore,
+			totalGuesses: usedGuesses,
+			totalWinners: winner,
+			highScore: finalScore,
+		});
 	}
-  
+
 	const updatedDoc = await getDoc(docRef);
 	return updatedDoc.data().count;
-  };
+};
 
 const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 	const [isMounted, setIsMounted] = useState(false);
@@ -101,20 +101,20 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 		const scoreDifference = targetScore - animatedScore;
 		const steps = duration / stepTime;
 		const stepSize = scoreDifference / steps;
-	
+
 		const intervalId = setInterval(() => {
-		  setAnimatedScore((prevScore) => {
-			const nextScore = prevScore + stepSize;
-			if ((stepSize > 0 && nextScore >= targetScore) || (stepSize < 0 && nextScore <= targetScore)) {
-			  clearInterval(intervalId);
-			  return targetScore;
-			}
-			return nextScore;
-		  });
+			setAnimatedScore((prevScore) => {
+				const nextScore = prevScore + stepSize;
+				if ((stepSize > 0 && nextScore >= targetScore) || (stepSize < 0 && nextScore <= targetScore)) {
+					clearInterval(intervalId);
+					return targetScore;
+				}
+				return nextScore;
+			});
 		}, stepTime);
-	
+
 		return () => clearInterval(intervalId);
-	  }, [currentGameState.hints.points]);
+	}, [currentGameState.hints.points]);
 
 	useEffect(() => {
 		const targetBonus = currentGameState.life.remainingGuessCount * 25;
@@ -123,25 +123,25 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 		const bonusDifference = targetBonus - animatedBonus;
 		const steps = duration / stepTime;
 		const stepSize = bonusDifference / steps;
-	
+
 		const intervalId = setInterval(() => {
-		  setAnimatedBonus((prevBonus) => {
-			const nextBonus = prevBonus + stepSize;
-			if ((stepSize > 0 && nextBonus >= targetBonus) || (stepSize < 0 && nextBonus <= targetBonus)) {
-			  clearInterval(intervalId);
-			  return targetBonus;
-			}
-			return nextBonus;
-		  });
+			setAnimatedBonus((prevBonus) => {
+				const nextBonus = prevBonus + stepSize;
+				if ((stepSize > 0 && nextBonus >= targetBonus) || (stepSize < 0 && nextBonus <= targetBonus)) {
+					clearInterval(intervalId);
+					return targetBonus;
+				}
+				return nextBonus;
+			});
 		}, stepTime);
-	
+
 		return () => clearInterval(intervalId);
-	}, [currentGameState.life.remainingGuessCount]);  
+	}, [currentGameState.life.remainingGuessCount]);
 
 	const gameOverRef = useRef(null);
 
-  	const triggerGameOver = () => {
-    	setIsGuessCountUpdated(true);
+	const triggerGameOver = () => {
+		setIsGuessCountUpdated(true);
 	};
 
 	const todaysDate = useMemo(() => {
@@ -153,25 +153,25 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 
 	const highestScore = useMemo(() => {
 		if (gameHistory.scores.length > 0) {
-	  	return gameHistory.scores.reduce((highScore, game) => 
-			game.score > highScore ? game.score : highScore, 0);
+			return gameHistory.scores.reduce((highScore, game) =>
+				game.score > highScore ? game.score : highScore, 0);
 		}
 		return 0;
-  	}, [gameHistory]);
-  
-  	const averageScore = useMemo(() => {
-		if (gameHistory.scores.length > 0) {
-	  	let totalScore = gameHistory.scores.reduce((total, game) => total + game.score, 0);
-	  	return Math.round(totalScore / gameHistory.scores.length);
-		}
-		return 0;
-  	}, [gameHistory]);
+	}, [gameHistory]);
 
-	  const calculateStreaks = (scores) => {
+	const averageScore = useMemo(() => {
+		if (gameHistory.scores.length > 0) {
+			let totalScore = gameHistory.scores.reduce((total, game) => total + game.score, 0);
+			return Math.round(totalScore / gameHistory.scores.length);
+		}
+		return 0;
+	}, [gameHistory]);
+
+	const calculateStreaks = (scores) => {
 		let currentStreak = 0;
 		let longestStreak = 0;
 		let tempStreak = 0;
-	
+
 		for (let score of scores) {
 			if (score.score > 0) {
 				tempStreak++;
@@ -182,65 +182,65 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 			}
 			longestStreak = Math.max(longestStreak, tempStreak);
 		}
-	
+
 		return {
 			currentStreak,
 			longestStreak,
 		};
-	}	
+	}
 
 	useEffect(() => {
 		if (todaysDate !== currentGameState.date && game) {
-		  setCurrentGameState({
-			...initialGameState,
-			releaseDate: game.releaseDate,
-			date: todaysDate,
-			hasPlayed: false,
-		  });
+			setCurrentGameState({
+				...initialGameState,
+				releaseDate: game.releaseDate,
+				date: todaysDate,
+				hasPlayed: false,
+			});
 		} else if (isGameOver) {
-		  setCurrentGameState((prevState) => ({
-			...prevState,
-			hints: {
-			  publisher: true,
-			  developer: true,
-			  genre: true,
-			  platforms: true,
-			  modes: true,
-			  engine: true,
-			  metacritic: true,
-			  plot: true,
-			  boxArt: 0,
-			  points: prevState.hints.points,
-			},
-			life: {
-				guesses: prevState.life.guesses,
-				remainingGuessCount: prevState.life.remainingGuessCount,
-				hearts: prevState.life.hearts,
-			},
-		  }));
+			setCurrentGameState((prevState) => ({
+				...prevState,
+				hints: {
+					publisher: true,
+					developer: true,
+					genre: true,
+					platforms: true,
+					modes: true,
+					engine: true,
+					metacritic: true,
+					plot: true,
+					boxArt: 0,
+					points: prevState.hints.points,
+				},
+				life: {
+					guesses: prevState.life.guesses,
+					remainingGuessCount: prevState.life.remainingGuessCount,
+					hearts: prevState.life.hearts,
+				},
+			}));
 		}
-	  }, [todaysDate, game, currentGameState.date, isGameOver]);
+	}, [todaysDate, game, currentGameState.date, isGameOver]);
 
 	useEffect(() => {
 		if (game && game.releaseDate !== currentGameState.releaseDate) {
-		  const existingGameEntry = gameHistory.scores.find(scoreEntry => scoreEntry.releaseDate === game.releaseDate);
-		  if (existingGameEntry) {
-			setCurrentGameState((prevState) => ({
-			  ...prevState,
-			  hints: {
-				...prevState.hints,
-				points: existingGameEntry.score
-			  }
-			}));
-			handleGameOver(existingGameEntry.score === 0);
-		  } else {
-			setCurrentGameState({
-			  ...initialGameState,
-			  releaseDate: game.releaseDate
-			});
-		  }
+			const existingGameEntry = gameHistory.scores.find(scoreEntry => scoreEntry.releaseDate === game.releaseDate);
+			if (existingGameEntry) {
+				setCurrentGameState((prevState) => ({
+					...prevState,
+					hints: {
+						...prevState.hints,
+						points: existingGameEntry.score
+					}
+				}));
+				handleGameOver(existingGameEntry.score === 0);
+			} else {
+				setCurrentGameState({
+					...initialGameState,
+					releaseDate: game.releaseDate
+				});
+			}
 		}
-	  }, [game, currentGameState.releaseDate, gameHistory.scores]);	  
+	}, [game, currentGameState.releaseDate, gameHistory.scores]);
 
 	useEffect(() => {
 		const updatedHearts = Array.from({ length: 4 }, (_, index) =>
@@ -260,9 +260,9 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 	useEffect(() => {
 		if (isGuessCountUpdated && currentGameState.life.remainingGuessCount === 0) {
 			handleGameOver(true);
-		  }
-		  setIsGuessCountUpdated(false);
-		}, [isGuessCountUpdated, currentGameState.life.remainingGuessCount]);
+		}
+		setIsGuessCountUpdated(false);
+	}, [isGuessCountUpdated, currentGameState.life.remainingGuessCount]);
 
 	useEffect(() => {
 		setIsMounted(true);
@@ -282,7 +282,7 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 		let finalScore = score + (currentGameState.life.remainingGuessCount * 25);
 		let usedGuesses = resetScore ? 4 : 4 - currentGameState.life.remainingGuessCount;
 
-		usedGuesses = usedGuesses === 0 ? 1 : usedGuesses;	
+		usedGuesses = usedGuesses === 0 ? 1 : usedGuesses;
 
 		setModalScore(finalScore);
 
@@ -291,31 +291,31 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 			date: todaysDate,
 			score: finalScore,
 		}]);
-	
+
 		setCurrentGameState((prevState) => {
 			const updatedGameState = {
-			  ...prevState,
-			  hints: {
-				publisher: true,
-				developer: true,
-				genre: true,
-				platforms: true,
-				modes: true,
-				engine: true,
-				metacritic: true,
-				plot: true,
-				boxArt: 0,
-				points: score,
-			  },
-			  hasPlayed: true,
+				...prevState,
+				hints: {
+					publisher: true,
+					developer: true,
+					genre: true,
+					platforms: true,
+					modes: true,
+					engine: true,
+					metacritic: true,
+					plot: true,
+					boxArt: 0,
+					points: score,
+				},
+				hasPlayed: true,
 			};
-			
+
 			// Save immediately to localStorage
 			window.localStorage.setItem("CURRENT_GAME_STATE", JSON.stringify(updatedGameState));
-			
+
 			return updatedGameState;
 		});
-	
+
 		setGameHistory((prevState) => ({
 			...prevState,
 			wins: resetScore ? prevState.wins : prevState.wins + 1,
@@ -323,18 +323,18 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 			currentStreak: currentStreak,
 			longestStreak: longestStreak,
 			scores: [
-			  ...prevState.scores,
-			  {
-				releaseDate: game.releaseDate,
-				date: todaysDate,
-				score: finalScore,
-			  },
+				...prevState.scores,
+				{
+					releaseDate: game.releaseDate,
+					date: todaysDate,
+					score: finalScore,
+				},
 			],
 		}));
 
 		await trackPlayer(finalScore, usedGuesses, !resetScore);
 
-    	setPlayerStatsUpdated(prev => !prev);
+		setPlayerStatsUpdated(prev => !prev);
 
 		if (typeof twq === "function") {
 			twq('event', 'tw-ou7tq-ou7tq', {
@@ -342,10 +342,10 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 
 			});
 		}
-	
+
 		setModalScore(finalScore);
 		setIsModalVisible(true);
-	};	
+	};
 
 	const onRevealHint = (points) => {
 		setCurrentGameState((prevState) => ({
@@ -361,7 +361,6 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 		const cleanedGuess = normaliseString(stripBrackets(guess));
 		const cleanedGameTitle = normaliseString(stripBrackets(game.title));
 
-		console.log(`Guess: ${cleanedGuess}, Game Title: ${cleanedGameTitle}`);
 		console.log(cleanedGuess === cleanedGameTitle);
 
 		if (cleanedGuess === cleanedGameTitle) {
@@ -370,24 +369,24 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 			setCurrentGameState((prevState) => {
 				let updatedGuesses = [...prevState.life.guesses, guess];
 				let updatedRemainingGuessCount = prevState.life.remainingGuessCount - 1;
-	
+
 				return {
 					...prevState,
 					life: {
 						...prevState.life,
 						guesses: updatedGuesses,
 						remainingGuessCount: updatedRemainingGuessCount,
-					},				
+					},
 				};
 			});
-	
+
 			setIsWrongGuess(true);
 			setTimeout(() => {
 				setIsGuessCountUpdated(true);
 				setIsWrongGuess(false);
 			}, 500);
 		}
-	};	
+	};
 
 	return isMounted ? (
 		<div className={styles.container}>
@@ -398,26 +397,26 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 				</p>
 			</div> */}
 			<div className={`${styles.stats} ${styles.statsMobile}`}>
-					<div className={styles.heartsContainer}>
-						{currentGameState.life.hearts.map((heartSrc, index) => (
-							<Image
-								key={index}
-								src={heartSrc}
-								alt="Heart"
-								width={30}
-								height={30}
-								className={
-									isWrongGuess &&
+				<div className={styles.heartsContainer}>
+					{currentGameState.life.hearts.map((heartSrc, index) => (
+						<Image
+							key={index}
+							src={heartSrc}
+							alt="Heart"
+							width={30}
+							height={30}
+							className={
+								isWrongGuess &&
 									index === currentGameState.life.remainingGuessCount
-										? styles.blink
-										: ""
-								}
-							/>
-						))}
-					</div>
-					<p>Bonus: {Math.round(animatedBonus)}</p>
-					<p>Score: {Math.round(animatedScore)}</p>
+									? styles.blink
+									: ""
+							}
+						/>
+					))}
 				</div>
+				<p>Bonus: {Math.round(animatedBonus)}</p>
+				<p>Score: {Math.round(animatedScore)}</p>
+			</div>
 			<SearchBar onSubmit={handleGuess} isGameOver={isGameOver} />
 			{game && currentGameState && (
 				<GameCard
@@ -462,7 +461,7 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 									height={30}
 									className={
 										isWrongGuess &&
-										index === currentGameState.life.remainingGuessCount
+											index === currentGameState.life.remainingGuessCount
 											? styles.blink
 											: ""
 									}
@@ -482,13 +481,13 @@ const ScoreSystem = ({ game, gameHistory, setGameHistory }) => {
 				</div>
 			</div>
 			<GameOverModal
-  				show={isModalVisible}
-  				gameTitle={game ? game.title : ""}
-  				score={modalScore}
-  				gamesPlayed={gameHistory.games}
-  				highestScore={highestScore}
-  				averageScore={averageScore}
-  				gamesWon={gameHistory.wins}
+				show={isModalVisible}
+				gameTitle={game ? game.title : ""}
+				score={modalScore}
+				gamesPlayed={gameHistory.games}
+				highestScore={highestScore}
+				averageScore={averageScore}
+				gamesWon={gameHistory.wins}
 				remainingGuesses={currentGameState.life.remainingGuessCount}
 				releaseDate={game.releaseDate}
 				globalAverageScore={globalAverageScore}
